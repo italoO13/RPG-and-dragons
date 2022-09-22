b3dcc871c2f1a4956f47a591c5ff7c04e2d0c06f
Italo Iveldo <italoiveldoluiz@gmail.com>
2022-09-22 16:29:35 -0300

    feat: importação das classes especializadas de arquetipo no arquivo index.ts

--- src/Archetypes/index.ts
+++ src/Archetypes/index.ts
@@ -1,3 +1,13 @@
 import Archetype from './Archetype';
+import Ranger from './Ranger';
+import Mage from './Mage';
+import Warrior from './Warrior';
+import Necromancer from './Necromance';
 
 export default Archetype;
+export {
+  Ranger, 
+  Mage,
+  Warrior, 
+  Necromancer,
+};
