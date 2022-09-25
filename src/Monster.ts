f3c1c26ba43601ee42a849c99a1b2379964de094
Italo Iveldo <italoiveldoluiz@gmail.com>
2022-09-24 21:22:48 -0300

    feat: class Monster que representa os inimigos gerais

---
+++ src/Monster.ts
@@ -0,0 +1,39 @@
+import { SimpleFighter } from './Fighter';
+
+class Monster implements SimpleFighter {
+  private _lifePoints: number;
+  private _strength: number;
+
+  constructor(lifePoints = 85, strength = 63) {
+    this._lifePoints = lifePoints;
+    this._strength = strength;
+  }
+
+  get lifePoints():number {
+    return this._lifePoints;
+  }
+
+  get strength():number {
+    return this._strength;
+  }
+
+  private damagelifePoints(damage: number) {
+    const life = this._lifePoints - damage;
+    if (life <= 0) {
+      this._lifePoints = -1;
+    } else {
+      this._lifePoints = life;
+    }
+  }
+
+  attack(enemy: SimpleFighter): void {
+    enemy.receiveDamage(this._strength);
+  }
+
+  receiveDamage(attackPoints: number): number {
+    this.damagelifePoints(attackPoints);
+    return this._lifePoints;
+  }
+}
+
+export default Monster;
